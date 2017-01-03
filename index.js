7ebd0b7fd7c68579ca47e6696cdb2431d04fcc08
Ada Rose Edwards <ada@ada.is>
2017-01-03 11:11:33 +0000

    PORT

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const WebSocketServer = require('ws').Server;
 const wss = new WebSocketServer({ server: server });
 const express = require('express');
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 let i = 1;
 
